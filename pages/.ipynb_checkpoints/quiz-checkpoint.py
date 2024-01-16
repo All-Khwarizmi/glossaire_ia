e8c8ff4861dcd190408321292cfdf4e34ff44eaa
Jason Suarez <swarecito@gmail.com>
2024-01-16 12:10:37 +0100

    Add SQLite configuration and store words

--- pages/.ipynb_checkpoints/quiz-checkpoint.py
+++ pages/.ipynb_checkpoints/quiz-checkpoint.py
@@ -12,9 +12,10 @@ st.title("Second Page")
 # %% ../quiz.ipynb 3
 # Create the SQL connection to pets_db as specified in your secrets file.
 from sqlalchemy import text
-conn = st.connection('words_db', type='sql')
+conn = st.connection('pets_db', type='sql')
 
 # Insert some data with conn.session.
+with conn.session as s:
    
 
 # Query and display the data you inserted
